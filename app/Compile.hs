e281b2dd0a648be5f6897dc0a3bed41694b2036a
Peter Achinger <peter.achinger@yahoo.com>
2025-05-15 15:11:40 +0200

    Add Starter Assembly Code

--- app/Compile.hs
+++ app/Compile.hs
@@ -4,12 +4,15 @@ module Compile
   )
 where
 
-import Compile.AAsm (codeGen)
+import Compile.InstructionSelection (codeGen)
 import Compile.Parser (parseAST)
 import Compile.Semantic (semanticAnalysis)
 import Compile.Liveness (tagLines, liveness)
-import Control.Monad.IO.Class
+import Compile.RegisterAllocation(allocateRegisters)
+import Control.Monad.IO.Class (MonadIO (liftIO))
 import Error (L1ExceptT)
+import Data.List (intercalate)
+
 
 data Job = Job
   { src :: FilePath,
@@ -20,15 +23,19 @@ data Job = Job
 compile :: Job -> L1ExceptT ()
 compile job = do
   ast <- parseAST $ src job
-  liftIO $ writeFile (out job) (show ast)
-  liftIO $ appendFile (out job) "\n"
+  -- liftIO $ writeFile (out job) (show ast)
+  -- liftIO $ appendFile (out job) "\n"
   semanticAnalysis ast
   let code = codeGen ast
-  liftIO $ appendFile (out job) (show code)
+  -- liftIO $ appendFile (out job) (show code)
   let taggedLines = tagLines code
-  liftIO $ appendFile (out job) "\n"
-  liftIO $ appendFile (out job) (show taggedLines)
+  -- liftIO $ appendFile (out job) "\n"
+  -- liftIO $ appendFile (out job) (show taggedLines)
   let live = liveness taggedLines
-  liftIO $ appendFile (out job) "\n"
-  liftIO $ appendFile (out job) (show live)
+  let output = allocateRegisters code live
+  starterCode <- liftIO $ readFile "res/starter_code"
+  liftIO $ writeFile (out job) starterCode
+  liftIO $ appendFile (out job) intercalate "\n" (map show output)
+  -- liftIO $ appendFile (out job) "\n"
+  -- liftIO $ appendFile (out job) (show live)
   return ()
