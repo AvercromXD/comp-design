bf29c399d87f638144d50c9a474b92a0f8631cb7
Ian <avercromxd@gmail.com>
2025-05-16 12:20:30 +0200

    Modified run.sh to catch error codes

--- run.sh
+++ run.sh
@@ -2,4 +2,23 @@
 
 # This script is supposed to run your compiler
 cabal run l1c-exec $1 output.s
+
+haskell_exit_code=$?
+
+# If the Haskell program failed, then exit with its error code
+if [ $haskell_exit_code -ne 0 ]; then
+    echo "Haskell program failed with exit code $haskell_exit_code. Aborting gcc compilation."
+    exit $haskell_exit_code
+fi
+
 gcc -o $2 output.s
+
+gcc_exit_code=$?
+
+if [ $gcc_exit_code -ne 0 ]; then
+    echo "GCC failed with exit code $gcc_exit_code."
+    exit $gcc_exit_code
+fi
+
+echo "Compilation succeeded."
+exit 0
